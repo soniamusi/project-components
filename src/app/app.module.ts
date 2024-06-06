2e2c792f9ab2730535d0647a1fd3da8504cdb96c
Sônia Muniz <soninhamusi@hotmail.com>
2024-06-06 12:44:51 -0400

    project-angular-16v

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardsModule } from './cards/cards.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    CardsModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
